a72881d66a2a433e94a56db30960146a356fb46b
Isaque-Gomes <116127894+Isaque-Gomes@users.noreply.github.com>
2023-02-09 12:07:43 -0300

    [M1S01] Ex 6 - Tabuada
    [Estrutura de Repetição]
    Crie um programa que receba um número inteiro, e a partir desse número exiba a tabuada do 0 até o 10 dele. Ex: Caso receba o número 2, a saída do programa deverá ser: 
    
    2 X 0 = 0
    2 X 1 = 2
    2 X 2 = 4
    2 X 3 = 6
    2 X 4 = 8
    2 X 5 = 10
    2 X 6 = 12
    2 X 7 = 14
    2 X 8 = 16
    2 X 9 = 18
    2 X 10 = 20

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Tabuada de 10
+int num;
+int calc;
+Console.WriteLine("Olá! Vamos fazer a tabuada de 10 de um número que você desejar!");
+Console.WriteLine("Digite um número que deseja fazer a tabuada:");
+num = int.Parse(Console.ReadLine());
+for(int i = 0; i < 11; i++){
+    calc = num * i;
+    Console.WriteLine($"{num} x {i} = {calc}");
+}
